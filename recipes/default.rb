99ea9954226466720271cd3c7a99e0629a2ad61c
Josh Cox <josh@webhosting.coop>
2014-11-06 11:40:27 -0600

    using vars in bash properly now

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,12 @@
 # Copyright 2014, Webhosting.coop
 #
 #
+ipa_admin_password = default["freeipa"]["ipa_admin_password"]
+hostname = default["freeipa"]["hostname"] 
+domain = default["freeipa"]["domain"] 
+dir_manager_password = default["freeipa"]["dir_manager_password"] 
+realm_name = default["freeipa"]["realm_name"]
+
 hostsfile_entry node['ipaddress'] do
     hostname  default["freeipa"]["hostname"]
     action    :create_if_missing
@@ -19,6 +25,6 @@ script "install freeipa" do
   group "root"
   cwd "/tmp"
   code <<-EOH
-  ipa-server-install -U --no-host-dns -a default["freeipa"]["ipa_admin_password"] --hostname=default["freeipa"]["hostname"] -n default["freeipa"]["domain"] -p default["freeipa"]["dir_manager_password"] -r default["freeipa"]["realm_name"]
+  ipa-server-install -U --no-host-dns -a #{ipa_admin_password} --hostname=#{hostname} -n #{domain} -p #{dir_manager_password} -r #{realm_name}
   EOH
 end
